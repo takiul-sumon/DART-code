b0fdd2c7535f3575da8e16e21112fcd7eb8b7431
Md Takiul Islam Sumon <117566937+takiul-sumon@users.noreply.github.com>
2024-01-04 19:56:48 +0600

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,33 @@
+//passing a value using constructor
+/*class Person{
+  int? id;
+  String? name;
+  Person(this.name,this.id)
+  { 
+   id=70;
+  }
+  
+    
+}
+
+void main()
+{ var v1=Person('Hasi',40);
+ print(v1.id);
+ print(v1.name);
+}
+*/
+//passing a value and string using a class 
+  class Sumon{
+  int id;
+  String name;
+  Sumon(this.id,this.name)
+  {
+    
+  ;}
+}
+void main()
+{
+  var v=Sumon(30,'Sumon');
+  print("${v.id},${v.name}");
+  
+}
